48e689ad3753a2f67d5dc5c4f38c400edc4d7c18
KamilaOlszewska <76682285+KamilaOlszewska@users.noreply.github.com>
2021-02-23 08:41:43 +0100

    zmiany

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
 const input = document.querySelectorAll("input");
+
 for (let el of input){
-    el.addEventListener("input", e=>
-    console.log(el))
+    el.addEventListener("input", e=>{
+    console.log(el.parentElement)
+    ;})
+    el.addEventListener("click",e=>{
+        el.parentElement.style.backgroundColor= "#aaa";
+    })
 }
